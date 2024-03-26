72ad52a14e33d61c849698a8780f4e28295b43bd
Diego Rodrigues Góes <diego353.goes@gmail.com>
2024-03-26 17:00:30 -0300

    Ajuste nas configs da PWA.

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,23 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'src/service-worker.js',
-    },
+    name: 'frontend_ifood', // Nome da sua PWA
+    short_name: 'ifood', // Nome curto
+    themeColor: '#ffffff', // Cor do tema
+    msTileColor: '#ffffff', // Cor da tile no Windows
+    startUrl: '/', // URL inicial
+    display: 'standalone', // Modo de exibição
+    icons: [
+      {
+        src: './public/img/icons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: './public/img/icons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
   },
 });
