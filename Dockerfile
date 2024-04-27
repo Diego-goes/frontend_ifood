4f27a8152a4f4a46b20367ffbef7238d555b1395
Diego Rodrigues Góes <83193074+Diego-goes@users.noreply.github.com>
2024-04-27 00:06:03 -0300

    Diego (#11)
    * Fix HTTPS webSocket issue

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use the official Node.js image as the base image
-FROM node:14
+FROM node:16-alpine
 
 # Set the working directory inside the container
 WORKDIR /app
@@ -9,18 +9,12 @@ COPY package*.json ./
 
 # Install dependencies
 RUN npm install
-RUN npm install -g mkcert
-# Cria o certificado SSL
-WORKDIR /app/certs
-RUN mkcert create-ca
-RUN mkcert create-cert
-WORKDIR /app
 
 # Copy the rest of the application code to the working directory
 COPY . .
 
 # Expose the port on which the application will run (adjust as needed)
-EXPOSE 8080
+EXPOSE $PORT
 
 # Define the command to start the application
 CMD ["npm", "run", "serve"]
--- vue.config.js
+++ vue.config.js
@@ -2,20 +2,24 @@ const fs = require('fs');
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  transpileDependencies: true,
   devServer: {
-    https: {
-      key: fs.readFileSync('./certs/cert.key'),
-      cert: fs.readFileSync('./certs/cert.crt'),
-    },
+    host: '0.0.0.0',
+    port: process.env.$PORT || 8080,
+    allowedHosts: [
+      'localhost',
+      'frontendhifood-production.up.railway.app'
+    ],
+    client: {
+      webSocketURL: 'auto://0.0.0.0:0/ws'
+    }
   },
   pwa: {
-    name: 'frontend_ifood', // Nome da sua PWA
-    short_name: 'ifood', // Nome curto
-    themeColor: '#ffffff', // Cor do tema
-    msTileColor: '#ffffff', // Cor da tile no Windows
-    startUrl: '/', // URL inicial
-    display: 'standalone', // Modo de exibição
+    name: 'frontend_ifood',
+    short_name: 'ifood', 
+    themeColor: '#ffffff', 
+    msTileColor: '#ffffff', 
+    startUrl: '/', 
+    display: 'standalone', 
     icons: [
       {
         src: './public/img/icons/android-chrome-192x192.png',
