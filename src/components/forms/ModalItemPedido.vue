f526266a5f39ec1872741cbd455c7eb8daba47b3
Diego Rodrigues Góes <diego353.goes@gmail.com>
2024-05-01 13:23:11 -0300

    Criação da URL para Pagina Estabelecimento e modal Item Pedido

---
+++ src/components/forms/ModalItemPedido.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- Aqui vai o HTML. Como se fosse o BODY -->
+    <div class="cardModalItemPedido">
+                
+    </div>
+</template>
+<script>
+export default{
+    name: "ModalItemPedido"
+}
+</script>
+<style scoped>
+/*Aqui vai o CSS*/
+
+</style>
