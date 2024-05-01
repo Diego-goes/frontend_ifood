27d8797083ad2e80cae0c8734223b349b1cdb7c6
Diego Rodrigues Góes <83193074+Diego-goes@users.noreply.github.com>
2024-05-01 13:24:41 -0300

    Diego (#14)
    * Criação da URL para Pagina Estabelecimento e modal Item Pedido

---
+++ src/views/PaginaEstabelecimento.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="viewPaginaEstabelecimento">
+        
+    </div>
+</template>
+<script>
+export default{
+    name: "PaginaEstabelecimento"
+}
+</script>
+<style scoped>
+
+</style>
