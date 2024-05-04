6929ace728315d71191d14f17e9949a3979c3783
TaisDebortoli <126207291+taisdebortoli21@users.noreply.github.com>
2024-05-04 20:22:07 -0300

    Tais (#21)
    * Finalização da integração da tela de cadastro; ajustes também no login usuario.
    
    * Integração front com front (tela de cadastro).
    
    * Finalização da integração da tela de cadastro; ajustes também no login usuario.
    
    * Criação de toda a pagina de estabelecimento; Adição de imagem "estrela"
    
    * Criação da tela de finalizar pedido; inclusão de 5 img dentro do assets; iniciei pag forma de pagamento.

--- src/views/FinalizarPedido.vue
+++ src/views/FinalizarPedido.vue
@@ -1,7 +1,80 @@
 <template>
-    <div>
-        <!-- Lembrar de sempre considerar essa div como o body da página -->
+    <div class="viewFinalizarPedido">  
+        <div class="container-esquerdo">
+            <h2>Finalize seu pedido</h2>
+            <div class="subtitulo">
+                <p>Entrega</p>
+                <hr>
+            </div>
+            <div class="endereco">
+                <img src="../assets/google-maps.png" alt="Maps" height="100px">
+                <div class="dados-endereco">
+                    <p>Endereço</p>
+                    <p class="texto-cinza">São Paulo, SP</p>
+                </div>
+                <input class="btn-opcao texto-vermelho" type="button" value="Trocar">
+                
+            </div>
+            
+            <hr>
+            <p>Pague pelo Site</p>
+                <div class="btn-pix">
+                    <img src="../assets/logoPix.png" alt="imagem pix">
+                    <div class="dados-pix">
+                        <p>Pague com Pix</p>
+                        <p> Use o QR Code ou copie e cole o código</p>
+
+                    </div>
+                </div>
+           
+            <p>Adicione um cartão no Hifood</p>
+            <div class="addcartao">
+                <div>
+                    <p>É prático, seguro e você não perde nenhum minuto a mais quando seu pedido chegar.</p>
+                    <input type="button"  value="Adicionar novo cartão">
+                </div>
+                <img src="../assets/formaPagamento.png" alt="Forma de pagamento">
+            </div>
+            <hr>
+            <input type="button" value="Fazer pedido">
+
+        </div>
+        <div class="container-direito">
+            <p class="texto-cinza">Seu pedido em</p>
+            <p>Nome do restaurante</p>
+            <small>categoria</small>
+            <hr>
+            <div class="card-itemPedido">
+                <div class="dados-pedido">
+                    <p>nome do prato</p>
+                    <p>R$ 00,00</p>
+        
+                </div>
+                <div class="opcoes-itemPedido">
+                    <input  class="btn-opcao texto-vermelho"  type="button" value="Editar">
+                    <input  class="btn-opcao texto-cinza" type="button" value="Remover">
+                </div>
+                <hr>
+            </div> 
+            <div class="valores-pedido">
+                <div  class="texto-cinza">
+                    <p>Subtotal</p>
+                    <p>R$ 00,00</p>
+                </div>
+
+                <div  class="texto-cinza">
+                    <p>Taxa de entrega</p>
+                    <p>Grátis</p>
+                </div>
+
+                <div>
+                    <p>Total</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>    
+        </div>    
     </div>
+    
 </template>
 <script>
 export default {
@@ -10,6 +83,114 @@ export default {
 </script>
 <style scoped>
 
-    /*Css vai aqui*/
+.viewFinalizarPedido{
+    display: flex;
+
+}
+
+.container-esquerdo{
+    display: flex;
+    text-align: left;
+    flex-direction: column;
+    
+}
+
+.container-esquerdo h2{
+    font-size: 2.5rem;
+}
+
+.subtitulo{
+    width: fit-content;
+}
+
+.subtitulo p{
+    color:#EA1D2C;
+    font-weight: 550;
+    margin-bottom: 0;
+   
+}
+.subtitulo hr{
+    width: 100%;
+    border: 0.15rem solid #EA1D2C;
+
+}
+
+.endereco{
+    display: flex;
+}
+
+.endereco img{
+    width: 5vw;
+    object-fit: contain;
+}
+
+.endereco p{
+    margin: 0;
+}
+
+.dados-endereco p:nth-child(1){
+    font-weight: 550;
+}
+
+.btn-opcao{
+    height: min-content;
+    background-color: transparent; 
+    border: none;
+
+}
+
+
+
+
+
+
+
+
+
+
+.container-direito{
+    display: flex;
+    flex-direction: column;
+    
+}
+
+
+
+.btn-pix{
+    display: flex;
+}
+
+.dados-pix{
+    
+}
+
+.addcartao{
+    display: flex;
+}
+
+.dados-pedido{
+    display: flex;
+}
+
+.valores-pedido div{
+    display: flex;
+    justify-content: space-between;
+}
+
+
+.texto-cinza{
+    color: #717171;
+
+}
+
+.texto-vermelho{
+    color: #EA1D2C;
+}
+
+
+*{
+    border: 1px solid red;
+}
+    
 
 </style>
--- src/views/FormasPagamento.vue
+++ src/views/FormasPagamento.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-        <!-- Lembrar de sempre considerar essa div como o body da página -->
+    <!-- Lembrar de sempre considerar essa div como o body da página -->
+    <div class="container">
+        <div class="meio-pagamento">
+            <p class="escolher-pagamento">Escolha um meio de pagamento</p>
+                <div class="cartao">
+                    <img src="../assets/cartaoCredito.png" alt="credito">
+                    <p class="credito">Crédito</p>
+                    <img src="../assets/cartaoDebito.png" alt="credito">
+                    <p class="debito">Débito</p>
+
+                </div>
+
+        </div>
+
     </div>
 </template>
 <script>
@@ -10,6 +22,46 @@ export default {
 </script>
 <style scoped>
 
-    /*Css vai aqui*/
+.container{
+    display: flex;
+    width: 100%;
+    flex-direction: column; 
+    
+}
+
+.escolher-pagamento{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    margin-top: 30vh;
+    color: #3e3e3e;
+}
+
+.cartao{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+}
+
+.credito{
+    border: 1px solid #3e3e3e;
+    padding: 20px 250px;
+
+}
+
+
+.debito{
+    border: 1px solid #3e3e3e;
+    padding: 20px 250px;
+
+}
+
+.cartao img{
+    height: 50px;
+}
+
 
 </style>
