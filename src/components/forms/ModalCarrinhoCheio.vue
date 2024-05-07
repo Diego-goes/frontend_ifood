133ef1a4c79c0ef48a4e22d7f5e26b2f0ddb7ce0
Diego Rodrigues Góes <83193074+Diego-goes@users.noreply.github.com>
2024-05-07 20:30:37 -0300

    Diego (#27)
    * Rotas para as paginas AcompanharPedido, FinalizarPedido e FormasPagamento implementadas.
    
    * Ajuste de navegação e imagem default no cardEstabelecimento; Criação do CardProduto; Ajuste no layout do Slider (antigo atrapalhava o hover); Ajuste do Z-index em modalItemPedido; Integração com a API e componentização do HTML na PaginaEstabelecimento; Refatoração para puxarDados realocada para as utils/funcsGerais, ajuste na páginaLogada.
    
    * Ajuste em puxarDados na parte de autorização; componente de Hello World foi para a pasta de Teste
    
    * Rotas para desenvolvimento dos modais Carrinho Cheio, Carrinho Vazio e Perfil.

---
+++ src/components/forms/ModalCarrinhoCheio.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default{
+    name:'ModalCarrinhoCheio',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped></style>
