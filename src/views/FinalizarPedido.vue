9164fa5f805822606bb712b9040d4b41e5fb35e9
TaisDebortoli <taisdebortoli8@gmail.com>
2024-05-07 20:35:05 -0300

    Conclusão de página finalizar pedido; adaptação da pagina forma de pagamento.

--- src/views/FinalizarPedido.vue
+++ src/views/FinalizarPedido.vue
@@ -16,10 +16,11 @@
                 
             </div>
             
-            <hr>
-            <p>Pague pelo Site</p>
+            <hr class="linha-divisão">
+            <p class="pague-pelo-site">Pague pelo site</p>
+
                 <div class="btn-pix">
-                    <img src="../assets/logoPix.png" alt="imagem pix">
+                    <img class="img-pix" src="../assets/logoPix.png" alt="imagem pix">
                     <div class="dados-pix">
                         <p>Pague com Pix</p>
                         <p> Use o QR Code ou copie e cole o código</p>
@@ -27,26 +28,27 @@
                     </div>
                 </div>
            
-            <p>Adicione um cartão no Hifood</p>
+            <p class="add-cartao">Adicione um cartão no Hifood</p>
             <div class="addcartao">
                 <div>
-                    <p>É prático, seguro e você não perde nenhum minuto a mais quando seu pedido chegar.</p>
-                    <input type="button"  value="Adicionar novo cartão">
+                    <input type="button" value="Crédito" class="btn-cartao">
+                    <input type="button" value="Débito" class="btn-cartao">
+                    <p class="frase-pix">É prático, seguro e você não perde nenhum minuto a mais quando seu pedido chegar.</p>    
                 </div>
                 <img src="../assets/formaPagamento.png" alt="Forma de pagamento">
             </div>
             <hr>
-            <input type="button" value="Fazer pedido">
+            <input type="button" value="Fazer pedido" class="btn-fazer-pedido">
 
         </div>
         <div class="container-direito">
-            <p class="texto-cinza">Seu pedido em</p>
-            <p>Nome do restaurante</p>
-            <small>categoria</small>
+            <p class="texto-cinza">Seu pedido em...</p>
+            <p class="nome-restaurante">Nome do restaurante</p>
+            <small>Categoria</small>
             <hr>
             <div class="card-itemPedido">
                 <div class="dados-pedido">
-                    <p>nome do prato</p>
+                    <p>Nome do prato</p>
                     <p>R$ 00,00</p>
         
                 </div>
@@ -64,7 +66,7 @@
 
                 <div  class="texto-cinza">
                     <p>Taxa de entrega</p>
-                    <p>Grátis</p>
+                    <p id="gratis">Grátis</p>
                 </div>
 
                 <div>
@@ -85,6 +87,7 @@ export default {
 
 .viewFinalizarPedido{
     display: flex;
+    width: 90vw;
 
 }
 
@@ -97,10 +100,12 @@ export default {
 
 .container-esquerdo h2{
     font-size: 2.5rem;
+    color: #3e3e3e;
 }
 
 .subtitulo{
     width: fit-content;
+    margin-bottom: 10px;
 }
 
 .subtitulo p{
@@ -117,51 +122,112 @@ export default {
 
 .endereco{
     display: flex;
+    
 }
 
 .endereco img{
     width: 5vw;
     object-fit: contain;
+    margin-right: 15px;
 }
 
 .endereco p{
     margin: 0;
+    margin-right: 35vh;
+    margin-top: 18px;
+    
 }
 
 .dados-endereco p:nth-child(1){
     font-weight: 550;
+    
 }
 
 .btn-opcao{
     height: min-content;
     background-color: transparent; 
     border: none;
+    margin-top: 3vw;
+    font-size: 15px;
 
 }
 
+hr{
+    width: 100%;
+    border: 1px solid #f5f5f9;
+}
 
+.pague-pelo-site{
 
+    font-weight: 550;
+    color: #EA1D2C;
+    margin-top: 5vh;
 
+}
 
+.img-pix{
+    height: 50px; 
+    width: auto;
+    margin-top: 3vh;
+    margin-right: 2vw;
+}
 
+.btn-pix{
+    display: flex;   
+    border: 1px solid #f5f5f9;    
+    margin-top: 2vh;
 
+}
 
+.add-cartao{
+    font-weight: 550;
+    color: #EA1D2C;
+    margin-top: 7vh;  
+}
+
+.btn-cartao{
+    width: 100px; 
+    height: 30px;
+    border: none;
+    margin-right: 10px;
+    color: #3e3e3e;
+    margin-top: 20px;
+    border-radius: 2px;
+    cursor: pointer;
+}
+
+.frase-pix{
+    margin-top: 50px;
+    font-style: italic
+}
+
+.btn-fazer-pedido{
+    width: 300px; 
+    height: 50px;
+    border: none;
+    background-color: #EA1D2C;
+    color: white;
+    align-items: center;
+    margin-left: 25vh;
+    cursor: pointer;
+}
+
+/* kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk */
 
 
 .container-direito{
     display: flex;
     flex-direction: column;
+    width: 40vw;
+    margin-top: 5%;
+    border: 20px solid white;
+    background-color: white;
+    box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.2); 
     
 }
 
-
-
-.btn-pix{
-    display: flex;
-}
-
-.dados-pix{
-    
+.nome-restaurante{
+    font-size: 20px;
 }
 
 .addcartao{
@@ -170,6 +236,8 @@ export default {
 
 .dados-pedido{
     display: flex;
+    justify-content: space-between;
+
 }
 
 .valores-pedido div{
@@ -177,6 +245,9 @@ export default {
     justify-content: space-between;
 }
 
+#gratis{
+    color: green;
+}
 
 .texto-cinza{
     color: #717171;
@@ -186,11 +257,6 @@ export default {
 .texto-vermelho{
     color: #EA1D2C;
 }
-
-
-*{
-    border: 1px solid red;
-}
-    
+ 
 
 </style>
