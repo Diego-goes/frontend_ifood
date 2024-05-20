3724cd675de3bc3908b266293035858aa8f8e196
Diego Rodrigues Góes <diego353.goes@gmail.com>
2024-05-19 22:45:44 -0300

    Criação da rota para editar card de codverif

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import PaginaEstabelecimento from "@/views/PaginaEstabelecimento.vue";
 import AcompanharPedido from "@/views/AcompanharPedido.vue";
 import FinalizarPedido from "@/views/FinalizarPedido.vue";
 import ModalEndereco from "@/components/forms/ModalEndereco.vue";
-
+import CardValidarCelular from "@/components/forms/CardValidarCelular.vue";
 const routes = [
   {
     path: "/",
@@ -18,6 +18,11 @@ const routes = [
     path: "/modalEndereco",
     name: "modalEnderecoRt",
     component: ModalEndereco,
+  },
+  {
+    path: "/codVerif",
+    name: "codVerifRt",
+    component: CardValidarCelular,
   },
   {
     path: "/paginaEstabelecimento",
