61b4003dc1f2cd9cda3e802c3ae02b5d9803113e
natominaga <110700048+natominaga@users.noreply.github.com>
2024-05-30 22:02:15 +0000

    tabela de relatorio

--- src/components/forms/ModalRelatorio.vue
+++ src/components/forms/ModalRelatorio.vue
@@ -29,9 +29,110 @@
                 </div>
             </div>
         </div>
-        <div>
-            <div>
-
+        <div class="info-relatorio">
+            <div class="tabela-relatorio">
+                <table border="1">
+                    <tr>
+                        <th>Restaurante</th>
+                        <th>Trabalho</th>
+                        <th>Casa</th>
+                        <th>Outros</th>
+                        <th>Período Inicial</th>
+                        <th>Período Final</th>
+                        <th>N° de Pedidos</th>
+                        <th>Faturamento</th>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                    <tr>
+                        <td>Mc Donalds</td>
+                        <td>890</td>
+                        <td>1.000</td>
+                        <td>30</td>
+                        <td>10/03/2024</td>
+                        <td>30/05/2024</td>
+                        <td>1.920</td>
+                        <td>R$140.000</td>
+                    </tr>
+                </table>
             </div>
             <div>
 
@@ -110,4 +211,45 @@ label {
     border: none;
     color: white;
 }
+
+.tabela-relatorio {
+    width: 80%;
+    max-width: 750px;
+    margin: 20px;
+    background: #fcfcfc;
+    padding: 20px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+th, td {
+    padding: 12px;
+    text-align: center;
+    border-bottom: 1px solid #ddd;
+}
+
+th {
+    cursor: pointer;
+    font-size: 14px;
+    background-color:  rgb(218, 10, 10);
+    color: white;
+}
+
+td {
+    font-size: 13px;
+    background-color: white;
+}
+
+caption {
+    caption-side: top;
+    font-size: 1rem;
+    margin-bottom: 10px;
+    font-weight: bold;
+}
+
 </style>
